bb5aaabfe1174a1ade974152adccad915e22db0f
George Ciesinski <george.ciesin@gmail.com>
2022-08-28 18:30:27 -0400

    Created getComputerChoice function

--- javascript/rps.js
+++ javascript/rps.js
@@ -1,6 +1,21 @@
 // Rock Paper Scissors
 
 // getComputerChoice - return rock, paper or scissors
+function getComputerChoice() {
+    // Select random number between 1-3
+    randNum = Math.floor((Math.random() * 3) + 1);
+    // Pick selection depending on random number
+    let computerSelection = (
+        randNum == 1 ? "rock" : 
+        randNum == 2 ? "paper" : 
+        randNum == 3 ? "scissors" : 
+        null
+    );
+    return computerSelection;
+}
+
+console.log(getComputerChoice());
+
 
 // Round - playerSelection and computerSelection
 
