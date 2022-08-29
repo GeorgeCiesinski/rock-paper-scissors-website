baf60422f8a93a44d483dff3b4727754ea7a4bc9
George Ciesinski <george.ciesin@gmail.com>
2022-08-28 23:47:25 -0400

    Added score and round counting

--- javascript/rps.js
+++ javascript/rps.js
@@ -2,16 +2,23 @@
 
 let roundOver = false;
 let currentRound = 1;  // Start from round 1
+let playerScore = 0;
+let computerScore = 0;
+let tieGame = false;
 
 // Get Buttons
 let btnRock = document.getElementById("rock");
 let btnPaper = document.getElementById("paper");
 let btnScissors = document.getElementById("scissors"); 
+let btnNext = document.getElementById("next-round");
+let btnReset = document.getElementById("reset");
 
 // Add event listeners
 btnRock.addEventListener("click", makePlayerSelection.bind(this, "rock"));
 btnPaper.addEventListener("click", makePlayerSelection.bind(this, "paper"));
 btnScissors.addEventListener("click", makePlayerSelection.bind(this, "scissors"));   
+btnNext.addEventListener("click", nextRound);
+btnReset.addEventListener("click", reset);
 
 // Get the computer choice - return rock, paper or scissors
 function getComputerChoice() {
@@ -31,24 +38,32 @@ function makePlayerSelection(playerChoice) {
         computerChoice = getComputerChoice();
         checkWinner(playerChoice, computerChoice);
         endRound();
+    } else {
+        console.log("The round has ended! Click Next Round!"); 
     }
 }
 
+// Determine the winner and alter score
 function checkWinner(playerChoice, computerChoice) {
     if (playerChoice == computerChoice) {
-        // Tie
+        console.log("The game is a tie!");
+        tieGame = true;
     } else if (
         (playerChoice == "rock" && computerChoice == "scissors") || 
         (playerChoice == "paper" && computerChoice == "rock") || 
         (playerChoice == "scissors" && computerChoice == "paper")
     ) {
-        // Win
+        console.log("Player won!");
+        playerScore += 1;
+        document.getElementById("player-score").innerHTML = playerScore;
     } else if (
         (playerChoice == "rock" && computerChoice == "paper") || 
         (playerChoice == "paper" && computerChoice == "scissors") || 
         (playerChoice == "scissors" && computerChoice == "rock")
     ) {
-        // Lose
+        console.log("Player Lost!");
+        computerScore += 1;
+        document.getElementById("computer-score").innerHTML = computerScore;
     }
 }
 
@@ -56,14 +71,26 @@ function endRound() {
     roundOver = true;
 }
 
-// When Next Round clicked
-function newRound() {
-
+function nextRound() {
+    if (roundOver) {
+        if (!tieGame) {
+            currentRound += 1;
+            document.getElementById("current-round").innerHTML = currentRound;
+            tieGame = false;
+        }
+        roundOver = false;
+    } else {
+        console.log("The round is not over yet, plebian!");
+    }
+    
 }
 
-
-
-
-// Round - playerSelection and computerSelection
-
-// Game - Calls round function - play 5 rounds
+function reset() {
+    console.log("Reset game.");
+    // Reset Scores
+    document.getElementById("player-score").innerHTML = 0;
+    document.getElementById("computer-score").innerHTML = 0;
+    // Reset Rounds
+    document.getElementById("current-round").innerHTML = 1;
+    roundOver = false;
+}
