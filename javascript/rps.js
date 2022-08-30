94c9300aa844036706c2879189f23277848bb3d9
George Ciesinski <george.ciesin@gmail.com>
2022-08-29 23:52:38 -0400

    Added multiple features
    - Slot machine like animation for computer
    - Game Over condition
    - Game Over images for winner & loser
    - Modified layout to save space
    - Debugging for multiple issues

--- javascript/rps.js
+++ javascript/rps.js
@@ -1,10 +1,12 @@
 // Rock Paper Scissors
 
-let roundOver = false;
+let gameOver = false;
+let roundOver = false;  // Toggles if round ends
 let currentRound = 1;  // Start from round 1
 let playerScore = 0;
 let computerScore = 0;
-let tieGame = false;
+let tieGame = false;  // Toggles if round ties
+let computerChoice;  // Global computer choice variable
 
 // Get Buttons
 let btnRock = document.getElementById("rock");
@@ -23,24 +25,28 @@ btnReset.addEventListener("click", reset);
 // Get the computer choice - return rock, paper or scissors
 function getComputerChoice() {
     randNum = Math.floor((Math.random() * 3) + 1);
-    let computerChoice = (
+    computerChoice = (
         randNum == 1 ? "rock" : 
         randNum == 2 ? "paper" : 
         randNum == 3 ? "scissors" : 
         null
     );
-    return computerChoice;
 }
 
 // Makes player selection
 function makePlayerSelection(playerChoice) {
     if (!roundOver) {
-        computerChoice = getComputerChoice();
+        updatePlayerIcon(playerChoice);
+        getComputerChoice();
         checkWinner(playerChoice, computerChoice);
-        endRound();
+        roundOver = true;  // Ends the round
     } else {
         console.log("The round has ended! Click Next Round!"); 
     }
+}
+
+function updatePlayerIcon(playerChoice) {
+    document.getElementById("player-selection").src = "images/" + playerChoice + ".png";
 }
 
 // Determine the winner and alter score
@@ -67,30 +73,69 @@ function checkWinner(playerChoice, computerChoice) {
     }
 }
 
-function endRound() {
-    roundOver = true;
-}
-
 function nextRound() {
-    if (roundOver) {
-        if (!tieGame) {
+    // If round ends and the game isn't tied, check how many rounds played
+    if (roundOver && !tieGame) {
+        // If fewer than 5 rounds played, increment rounds and reset images/variables
+        if (currentRound < 5) {
             currentRound += 1;
             document.getElementById("current-round").innerHTML = currentRound;
-            tieGame = false;
+            document.getElementById("player-selection").src = "images/transparent.png";
+            roundOver = false;
+        // If 5 or more rounds played
+        } else if (currentRound >= 5) {
+            endGame();
         }
+    // If game tied, reset round
+    } else if (tieGame) {
+        tieGame = false;
         roundOver = false;
     } else {
         console.log("The round is not over yet, plebian!");
     }
-    
+}
+
+function endGame() {
+    gameOver = true;
+    if (playerScore > computerScore) {
+        document.getElementById("player-selection").src = "images/crown.png";
+        document.getElementById("computer-selection").src = "images/skull.png";
+    } else {
+        document.getElementById("player-selection").src = "images/skull.png";
+        document.getElementById("computer-selection").src = "images/crown.png";
+    }
 }
 
 function reset() {
     console.log("Reset game.");
-    // Reset Scores
-    document.getElementById("player-score").innerHTML = 0;
-    document.getElementById("computer-score").innerHTML = 0;
-    // Reset Rounds
-    document.getElementById("current-round").innerHTML = 1;
+    playerScore = 0;  // Reset Scores
+    document.getElementById("player-score").innerHTML = playerScore;
+    computerScore = 0;
+    document.getElementById("computer-score").innerHTML = computerScore;
+    document.getElementById("current-round").innerHTML = 1;  // Reset Rounds
     roundOver = false;
+    gameOver = false;
+    document.getElementById("player-selection").src = "images/transparent.png";  // Reset player selection image
 }
+
+let computerImage = 1;
+
+function rotateComputerImages() {
+    if (!roundOver && !gameOver) {
+        if (computerImage == 1) {
+            document.getElementById("computer-selection").src = "images/rock.png";
+            computerImage++;
+        } else if (computerImage == 2) {
+            document.getElementById("computer-selection").src = "images/paper.png";
+            computerImage++;
+        } else {
+            document.getElementById("computer-selection").src = "images/scissors.png";
+            computerImage = 1;
+        }
+    } else if (roundOver && !gameOver) {
+        document.getElementById("computer-selection").src = "images/" + computerChoice + ".png";
+    }
+}
+
+// Rotate images per interval
+setInterval(rotateComputerImages, 100);
